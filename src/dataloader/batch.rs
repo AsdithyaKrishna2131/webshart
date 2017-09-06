cc5603fefbf303518869b92a7397d850246b3339
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-09-06 17:15:00 +0800

    refactor: metadata

---
+++ src/dataloader/batch.rs
@@ -0,0 +1,38 @@
+use crate::dataloader::file_loading::file_http_client;
+use crate::discovery::{DatasetDiscovery, DiscoveredDataset};
+use crate::error::{Result, WebshartError};
+use futures::future::join_all;
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyDict, PyList};
+use std::sync::Arc;
+use tokio::runtime::Runtime;
+
+/// Batch read request for a single file
+#[derive(Debug, Clone)]
+pub struct FileReadRequest {
+    /// Dataset to read from
+    pub dataset_idx: usize,
+    /// Shard index within the dataset
+    pub shard_idx: usize,
+    /// File index within the shard
+    pub file_idx: usize,
+}
+
+/// Result of a batch operation
+#[derive(Debug)]
+pub enum BatchResult<T> {
+    Ok(T),
+    Err(String),
+}
+
+/// Batch operations handler
+pub struct BatchOperations {
+    runtime: Arc<Runtime>,
+}
+
+impl BatchOperations {
+    pub fn new() -> Self {
+        Self {
+            runtime: Arc::new(Runtime::new().expect("Failed to create Tokio runtime")),
+        }
+    }
