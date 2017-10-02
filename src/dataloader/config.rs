e0053a138f4ed9423771414d497ae905e2b4965b
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2017-10-02 09:15:00 +0800

    chore: bump bindings

--- src/dataloader/config.rs
+++ src/dataloader/config.rs
@@ -4,3 +4,6 @@ use pyo3::types::PyDict;
 #[derive(Debug, Clone)]
 pub struct DataLoaderConfig {
     pub load_file_data: bool,
+    pub max_file_size: u64,
+    pub buffer_size: usize,
+    pub chunk_size_mb: usize,
