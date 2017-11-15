f5d7217a0fa7c61bbf5519c3d21745db35fabac6
EmiColeman780 <309199564+EmiColeman780@users.noreply.github.com>
2017-11-15 15:07:00 +0800

    fix: prefetch

---
+++ src/dataloader/entry_types.rs
@@ -0,0 +1,19 @@
+use crate::{metadata::CaptionValue, FileInfo};
+use pyo3::prelude::*;
+use pyo3::types::{PyBytes, PyDict};
+
+#[pyclass(name = "TarFileEntry", from_py_object)]
+#[derive(Clone)]
+pub struct PyTarFileEntry {
+    pub path: String,
+    pub offset: u64,
+    pub size: u64,
+    pub data: Vec<u8>,
+    pub width: Option<u32>,
+    pub height: Option<u32>,
+    pub aspect: Option<f32>,
+    pub json_path: Option<String>,
+    pub json_data: Option<Vec<u8>>,
+    pub captions: Option<CaptionValue>,
+    pub json_metadata: Option<serde_json::Value>,
+    pub shard_idx: Option<usize>,
