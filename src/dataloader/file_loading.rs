c8f86fb8a32473fe94e2c8f233425de54fc89b19
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2018-01-18 10:59:00 +0800

    refactor: tidy metadata

--- src/dataloader/file_loading.rs
+++ src/dataloader/file_loading.rs
@@ -10,3 +10,9 @@ use tokio::runtime::Runtime;
 pub trait FileLoader: Send + Sync {
     fn load_file(&self, file_info: &FileInfo) -> Result<Vec<u8>>;
 }
+
+pub(crate) fn file_http_client() -> Result<reqwest::Client> {
+    static CLIENT: OnceLock<reqwest::Client> = OnceLock::new();
+
+    if let Some(client) = CLIENT.get() {
+        return Ok(client.clone());
