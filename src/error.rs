15c48b7c7859caabf181bd3864c64e61c001db78
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2018-10-03 18:13:00 +0800

    feat: add streaming

--- src/error.rs
+++ src/error.rs
@@ -6,3 +6,7 @@ use thiserror::Error;
 pub enum WebshartError {
     #[error("IO error: {0}")]
     Io(#[from] std::io::Error),
+
+    #[error("JSON parsing error: {0}")]
+    Json(#[from] serde_json::Error),
+
