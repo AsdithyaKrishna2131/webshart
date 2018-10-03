96003137927d667bbb1f52976018557218944ea7
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2018-10-03 18:05:00 +0800

    feat: add dataloader

--- src/error.rs
+++ src/error.rs
@@ -2,3 +2,7 @@ use pyo3::exceptions::PyException;
 use pyo3::PyErr;
 use thiserror::Error;
 
+#[derive(Error, Debug)]
+pub enum WebshartError {
+    #[error("IO error: {0}")]
+    Io(#[from] std::io::Error),
