875cadd9ec1dcfd519980d2c6be053ff13807665
camillejackson <309327822+camillejackson@users.noreply.github.com>
2018-10-30 16:34:00 +0800

    fix: correct prefetch

--- src/extract.rs
+++ src/extract.rs
@@ -188,3 +188,28 @@ impl MetadataExtractor {
 
                 // Process shards in parallel
                 let semaphore = Arc::new(tokio::sync::Semaphore::new(max_workers));
+                let futures = shards.into_iter().map(|shard| {
+                    let sem = semaphore.clone();
+                    let checkpoint = checkpoints.get(&shard.name).cloned();
+                    let token = self.hf_token.clone();
+                    let dest = destination.to_string();
+                    let checkpoint_dir = checkpoint_dir.map(|s| s.to_string());
+                    let extractor = self.clone();
+                    let mp = multi_progress.clone();
+
+                    async move {
+                        let _permit = sem.acquire().await.unwrap();
+                        let result = extractor
+                            .process_shard(
+                                shard.clone(),
+                                checkpoint,
+                                &dest,
+                                checkpoint_dir.as_deref(),
+                                token,
+                                mp,
+                            )
+                            .await;
+                        result
+                    }
+                });
+
