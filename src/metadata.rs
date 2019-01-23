8a1ad23c68dfd5797b3a4fa8f325ef49ee46ae33
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2019-01-23 13:12:00 +0800

    refactor: shards

---
+++ src/metadata.rs
@@ -0,0 +1,65 @@
+use crate::discovery::DiscoveredDataset;
+use crate::error::{Result, WebshartError};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::{HashMap, HashSet};
+use std::time::Duration;
+
+/// Caption metadata extracted from paired JSON sidecars.
+///
+/// This serializes as either a single string or a list of strings so Python
+/// callers see `captions: str | list[str] | None`.
+#[derive(Debug, Clone, Serialize, Deserialize)]
+#[serde(untagged)]
+pub enum CaptionValue {
+    Single(String),
+    Multiple(Vec<String>),
+}
+
+impl CaptionValue {
+    pub fn first(&self) -> Option<&str> {
+        match self {
+            Self::Single(text) => Some(text.as_str()),
+            Self::Multiple(captions) => captions.first().map(String::as_str),
+        }
+    }
+}
+
+/// Information about a single file within a tar shard
+#[derive(Debug, Clone, Serialize, Deserialize)]
+pub struct FileInfo {
+    /// Name/path of the file (optional in JSON, as it may be the HashMap key)
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    #[serde(alias = "fname", alias = "filename")]
+    pub path: Option<String>,
+
+    /// Offset within the tar file
+    pub offset: u64,
+
+    /// Length of the file in bytes
+    #[serde(alias = "size")]
+    pub length: u64,
+
+    /// SHA256 hash of the file (optional)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub sha256: Option<String>,
+
+    /// Image width in pixels (optional)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub width: Option<u32>,
+
+    /// Image height in pixels (optional)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub height: Option<u32>,
+
+    /// Image aspect ratio (width / height) (optional)
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub aspect: Option<f32>,
+
+    /// Path of a paired JSON metadata file inside the same tar shard.
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    pub json_path: Option<String>,
+
+    /// Offset of the paired JSON metadata file inside the tar shard.
+    #[serde(skip_serializing_if = "Option::is_none", default)]
+    pub json_offset: Option<u64>,
