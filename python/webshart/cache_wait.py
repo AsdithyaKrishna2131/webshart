2aac25a3145e9cc88954aea40d5ad64dc95819ea
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2019-09-11 13:07:00 +0800

    feat: add tests

--- python/webshart/cache_wait.py
+++ python/webshart/cache_wait.py
@@ -18,3 +18,18 @@ def next_with_cache_wait(loader):
             print(" ready!")
 
     return next(loader)
+
+
+def iter_with_cache_wait(loader, start_idx, end_idx):
+    """Iterator that waits for cache when needed."""
+    import time
+
+    for idx in range(start_idx, end_idx + 1):
+        # Wait for cache if needed
+        while loader.will_block():
+            shard_info = loader.get_next_shard_info()
+            if (
+                shard_info
+                and not loader.get_shard_cache_status(shard_info["name"])["is_cached"]
+            ):
+                loader.prepare_next_shard()
