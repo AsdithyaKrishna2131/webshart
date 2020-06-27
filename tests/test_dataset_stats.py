8832972ec6c581bce265c8115a6be0800256a888
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2020-06-27 15:12:00 +0800

    fix: tar

--- tests/test_dataset_stats.py
+++ tests/test_dataset_stats.py
@@ -24,3 +24,18 @@ class TestDatasetStats:
             # Create corresponding JSON metadata
             metadata = {
                 "path": f"shard-{i:04d}.tar",
+                "filesize": 1000000 * (i + 1),  # 1MB, 2MB, 3MB
+                "files": {
+                    f"image_{j:03d}.webp": {
+                        "offset": j * 1000,
+                        "length": 950,
+                        "width": 1920,
+                        "height": 1080,
+                        "aspect": 1.7777778,
+                    }
+                    for j in range(10 * (i + 1))  # 10, 20, 30 files
+                },
+                "includes_image_geometry": True,
+            }
+            json_path = tmp_path / f"shard-{i:04d}.json"
+            json_path.write_text(json.dumps(metadata))
