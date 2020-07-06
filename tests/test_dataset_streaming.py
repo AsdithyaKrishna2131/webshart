83b8ef445f200945902371225a19a91f987fe162
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2020-07-06 14:53:00 +0800

    chore: eviction

---
+++ tests/test_dataset_streaming.py
@@ -0,0 +1,44 @@
+# test_tar_dataloader.py
+import pytest
+import webshart
+import tempfile
+import json
+import os
+import tarfile
+from pathlib import Path
+import io
+
+
+@pytest.fixture
+def mock_dataset_dir():
+    """Create a temporary directory with proper webshart-compatible dataset."""
+    with tempfile.TemporaryDirectory() as tmpdir:
+        # Create mock tar files with proper structure
+        for i in range(3):
+            shard_name = f"shard_{i:04d}"
+            tar_path = Path(tmpdir) / f"{shard_name}.tar"
+
+            # Create a proper tar file
+            with tarfile.open(tar_path, "w") as tar:
+                # Add some dummy files
+                for j in range(10):
+                    filename = f"{shard_name}_{j:06d}.jpg"
+                    # Create dummy image data
+                    data = f"FAKE_JPEG_DATA_SHARD{i}_FILE{j}".encode() * 100
+
+                    # Create tarinfo
+                    info = tarfile.TarInfo(name=filename)
+                    info.size = len(data)
+
+                    # Add to tar
+                    tar.addfile(info, io.BytesIO(data))
+
+            # Create metadata JSON that matches the tar structure
+            metadata = {"filesize": os.path.getsize(tar_path), "files": {}}
+
+            # Read the tar to get actual offsets
+            with tarfile.open(tar_path, "r") as tar:
+                offset = 0
+                for member in tar:
+                    if member.isfile():
+                        # In tar files, the header is 512 bytes, followed by data rounded up to 512 bytes
