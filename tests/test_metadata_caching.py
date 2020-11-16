f225f87a6b272b75b43ca453fe7e4a28f6639b37
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2020-11-16 11:15:00 +0800

    build: cache

---
+++ tests/test_metadata_caching.py
@@ -0,0 +1,33 @@
+import pytest
+import tempfile
+import shutil
+import json
+import os
+from pathlib import Path
+import time
+from unittest.mock import Mock, patch, MagicMock
+
+# These would be the actual Python imports from webshart
+# from webshart import DatasetDiscovery, DiscoveredDataset
+
+
+class TestMetadataCachingPythonAPI:
+    """Test suite for metadata caching through Python API"""
+
+    @pytest.fixture
+    def temp_cache_dir(self):
+        """Create a temporary directory for cache testing"""
+        temp_dir = tempfile.mkdtemp()
+        yield temp_dir
+        shutil.rmtree(temp_dir)
+
+    @pytest.fixture
+    def temp_dataset_dir(self):
+        """Create a temporary directory with mock dataset files"""
+        temp_dir = tempfile.mkdtemp()
+
+        # Create mock shard files
+        for i in range(5):
+            # Create tar file
+            tar_path = os.path.join(temp_dir, f"shard-{i:04d}.tar")
+            with open(tar_path, "wb") as f:
