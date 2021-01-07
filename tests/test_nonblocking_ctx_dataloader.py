5fe5e1135e427ccb4402f612579811570da9b0fe
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-01-07 13:27:00 +0800

    style: clean up ci

---
+++ tests/test_nonblocking_ctx_dataloader.py
@@ -0,0 +1,26 @@
+# test_cache_wait.py
+import pytest
+import time
+import threading
+from unittest.mock import Mock, MagicMock, patch
+from webshart.cache_wait import CacheWaitContext
+
+
+class MockDataLoader:
+    """Mock dataloader for testing cache wait functionality."""
+
+    def __init__(self, entries, block_pattern=None):
+        self.entries = entries
+        self.block_pattern = block_pattern or []
+        self.index = 0
+        self.will_block_calls = 0
+        self.prepare_calls = []
+        self.current_shard_filename = "shard-0000.tar"
+        self._blocking = False
+        self._download_start_time = None
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.index >= len(self.entries):
