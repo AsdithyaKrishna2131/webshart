5ce13c41abbf42c35aa7d2f456ba3e5f64a0e788
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-02-24 10:07:00 +0800

    style: clean up streaming

--- tests/test_paired_datasets.py
+++ tests/test_paired_datasets.py
@@ -7,3 +7,13 @@ class FakeDataset:
     def __init__(self, shards):
         self.shards = shards
         self.num_shards = len(shards)
+
+    def list_samples_in_shard(self, shard_index):
+        return self.shards[shard_index]
+
+
+def test_pair_index_joins_by_stem_in_left_order():
+    left = FakeDataset([["b.mp3", "a.mp3"], ["nested/c.mp3"]])
+    right = FakeDataset([["a.flac"], ["nested/c.wav", "b.wav"]])
+
+    paired = webshart.PairedDataset(left, right)
