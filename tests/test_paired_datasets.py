a4aff56b7ea3a875223b2b64b1db8703b8bb0406
witthayaevans <309016693+witthayaevans@users.noreply.github.com>
2021-02-26 16:00:00 +0800

    fix: dataloader

--- tests/test_paired_datasets.py
+++ tests/test_paired_datasets.py
@@ -17,3 +17,13 @@ def test_pair_index_joins_by_stem_in_left_order():
     right = FakeDataset([["a.flac"], ["nested/c.wav", "b.wav"]])
 
     paired = webshart.PairedDataset(left, right)
+
+    assert [pair.key for pair in paired.list_pairs()] == ["b", "a", "nested/c"]
+    assert paired.get_pair(0).left.sample_index == 0
+    assert paired.get_pair(0).right == webshart.SampleLocation(1, 1, "b.wav")
+
+
+def test_pair_index_reports_mismatches_in_strict_mode():
+    paired = webshart.PairedDataset(
+        FakeDataset([["shared.mp3", "left-only.mp3"]]),
+        FakeDataset([["shared.mp3", "right-only.mp3"]]),
