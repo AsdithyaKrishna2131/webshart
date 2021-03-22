337733389838d9601ef21c78c4f22fc2fc20778c
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-03-22 15:15:00 +0800

    build: cache

--- examples/dataloader.py
+++ examples/dataloader.py
@@ -6,3 +6,9 @@ import pickle
 hf_token = get_token()
 dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel", hf_token=hf_token)
 loader = TarDataLoader(dataset)
+
+# Resume from checkpoint if it exists
+checkpoint_file = "dataloader_state.pkl"
+if os.path.exists(checkpoint_file):
+    with open(checkpoint_file, "rb") as f:
+        state = pickle.load(f)
