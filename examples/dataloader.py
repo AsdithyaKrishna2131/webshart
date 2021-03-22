aee2edcfbabdcac5d0d1b4010fc39066a09814a5
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-03-22 15:03:00 +0800

    feat: shards

--- examples/dataloader.py
+++ examples/dataloader.py
@@ -1,3 +1,8 @@
 from webshart import TarDataLoader, discover_dataset
 from huggingface_hub import get_token
 import os
+import pickle
+
+hf_token = get_token()
+dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel", hf_token=hf_token)
+loader = TarDataLoader(dataset)
