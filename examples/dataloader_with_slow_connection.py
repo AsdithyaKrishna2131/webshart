5c3168b6582f076cd80a0a91bd5023596a8fdb1d
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-05-14 11:18:00 +0800

    feat: tar

--- examples/dataloader_with_slow_connection.py
+++ examples/dataloader_with_slow_connection.py
@@ -3,3 +3,5 @@ from tqdm import tqdm
 from webshart import discover_dataset, TarDataLoader, CacheWaitContext
 
 
+# Create dataloader with shard caching enabled
+dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel")
