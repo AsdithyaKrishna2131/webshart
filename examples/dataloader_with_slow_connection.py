baaf1910ccade26f9978addd6c2aac70dd3737ea
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-05-15 09:12:00 +0800

    test: cover metadata

--- examples/dataloader_with_slow_connection.py
+++ examples/dataloader_with_slow_connection.py
@@ -5,3 +5,5 @@ from webshart import discover_dataset, TarDataLoader, CacheWaitContext
 
 # Create dataloader with shard caching enabled
 dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel")
+dataset.enable_shard_cache("cache/", cache_limit_gb=50.0)
+
