014f83110244c9a9ff8c71c41c68ae2281b03d81
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-05-25 10:00:00 +0800

    chore: bump tar

--- examples/dataloader_with_slow_connection.py
+++ examples/dataloader_with_slow_connection.py
@@ -7,3 +7,4 @@ from webshart import discover_dataset, TarDataLoader, CacheWaitContext
 dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel")
 dataset.enable_shard_cache("cache/", cache_limit_gb=50.0)
 
+dataloader = TarDataLoader(dataset, batch_size=32, buffer_size=1000)
