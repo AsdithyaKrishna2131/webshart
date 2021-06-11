8c049738545f7a64219109dea6f09e032f86ff12
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-06-11 18:34:00 +0800

    refactor: streaming

--- examples/dataset_stats.py
+++ examples/dataset_stats.py
@@ -4,3 +4,6 @@ from huggingface_hub import get_token
 dataset = webshart.discover_dataset(
     "NebulaeWis/e621-2024-webp-4Mpixel", hf_token=get_token()
 )
+
+# Quick stats (instant, uses cached values if available)
+stats = dataset.get_stats()
