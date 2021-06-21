e7b8612cc6703234e71ca137f1cf8faeab39f151
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-06-21 14:57:00 +0800

    feat: metadata

--- examples/metadata_extractor.py
+++ examples/metadata_extractor.py
@@ -2,3 +2,5 @@ from webshart import MetadataExtractor
 from huggingface_hub import get_token
 
 # Create an extractor (optionally with HF token for private datasets)
+extractor = MetadataExtractor(hf_token=get_token())
+
