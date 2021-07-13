1103806bed8182da940904d29c5178a9b85f38c9
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2021-07-13 16:03:00 +0800

    feat: eviction

--- examples/retrieve_range.py
+++ examples/retrieve_range.py
@@ -1,3 +1,7 @@
 """
 ⚠️ This method uses several parallel connections to the source dataset, which will
 result in **429 Too Many Requests** responses from providers like Hugging Face Hub.
+It is intended for low-volume IO, with better solutions on the way for extended streaming.
+
+Alternatively, if you host your own minIO or similar, feel free to use this API, it is fast.
+"""
