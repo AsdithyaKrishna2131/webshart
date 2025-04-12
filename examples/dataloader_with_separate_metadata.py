f563eb80ce6f782bc92cdc49657e0756b179ba97
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2025-04-13 06:52:00 +0800

    build: ci

--- examples/dataloader_with_separate_metadata.py
+++ examples/dataloader_with_separate_metadata.py
@@ -8,3 +8,6 @@ dataset = discover_dataset(
     source="laion/conceptual-captions-12m-webdataset",
     metadata="webshart/conceptual-captions-12m-webdataset-metadata",
     # subfolder="data",
+    hf_token=hf_token,
+)
+loader = TarDataLoader(dataset)
