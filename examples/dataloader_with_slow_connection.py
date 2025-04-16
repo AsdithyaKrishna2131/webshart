77866635748831889be4baae8c2b6ee6aea38725
witthayaevans <309016693+witthayaevans@users.noreply.github.com>
2025-04-17 00:03:00 +0800

    style: clean up ci

--- examples/dataloader_with_slow_connection.py
+++ examples/dataloader_with_slow_connection.py
@@ -8,3 +8,7 @@ dataset = discover_dataset("NebulaeWis/e621-2024-webp-4Mpixel")
 dataset.enable_shard_cache("cache/", cache_limit_gb=50.0)
 
 dataloader = TarDataLoader(dataset, batch_size=32, buffer_size=1000)
+
+with CacheWaitContext(dataloader) as ctx:
+    for entry in ctx.iterate():
+        time.sleep(0.05)
