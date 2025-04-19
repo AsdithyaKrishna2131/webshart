88cd1b5c1fb19db286663690018389575a83c903
Merkle <62513+AsdithyaKrishna2131@users.noreply.github.com>
2025-04-20 02:12:00 +0800

    chore: bump shards

--- examples/aspect_bucketing.py
+++ examples/aspect_bucketing.py
@@ -26,3 +26,11 @@ for shard in shards_to_bucket:
         [shard],
         key="geometry-tuple",
         target_pixel_area=1024
+        ** 2,  # you can modify this to 512**2 or 768**2 etc for different target resolutions
+    )[0]["buckets"]
+    # for bucket_key in buckets_info.keys():
+    #     # each of the items has the original_size element, if the original was transformed.
+    #     print(f"Bucket {bucket_key}")
+    #     print(f"- {len(buckets_info[bucket_key])} items")
+    #     print(f"- Original size: {buckets_info[bucket_key][0]['original_size']}")
+    #     print(f"- Sample index: {buckets_info[bucket_key][0]['sample_idx']}")
